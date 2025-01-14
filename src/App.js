0e2b8af05a4f22c28e688b53356fcb77fe18a5be
koios07 <miguel.ardila1207@gmail.com>
2025-01-14 18:16:55 -0500

    instalacion webvitals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Asegúrate de importar Bootstrap
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-center my-4">Bienvenido a SAMG</h1>
       </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <h2>Columna 1</h2>
+            <p>Este es un ejemplo de contenido en la columna 1.</p>
+            <button className="btn btn-primary">Botón en Columna 1</button>
+          </div>
+          <div className="col-md-6 text-center">
+            <h2>Columna 2</h2>
+            <p>Este es un ejemplo de contenido en la columna 2.</p>
+            <button className="btn btn-secondary">Botón en Columna 2</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
